a2ffd902c05d2e6d273159164f6707a749ef79b3
Zhan <galymzhan.aitkaliyev@gmail.com>
2023-03-28 00:37:58 +0600

    finished set locale set maximumDate to dataPicker

---
+++ MyDaysApp/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  MyDaysApp
+//
+//  Created by Galymzhan A on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var resoultButton: UIButton!
+    @IBOutlet weak var dataPicker: UIDatePicker!
+    
+    private var numberOfDays:String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buttonView()
+        self.view.backgroundColor = UIColor(red: 203/255, green: 228/255, blue: 222/255, alpha: 1.0)
+        dataPicker.maximumDate = Date.now
+        infoLabel.textColor = UIColor(red: 14/255, green: 131/255, blue: 136/255, alpha: 1.0)
+        infoLabel.isHidden = true
+    }
+    
+    func buttonView() {
+        resoultButton.layer.cornerRadius = 12
+        resoultButton.backgroundColor = UIColor(red: 14/255, green: 131/255, blue: 136/255, alpha: 1.0)
+        resoultButton.setTitleColor(UIColor(red: 203/255, green: 228/255, blue: 222/255, alpha: 1.0), for: .normal)
+    }
+    
+    @IBAction func dataPicker(_ sender: UIDatePicker) {
+        
+        let range = sender.date..<Date.now
+        let russianLocale = Locale(identifier: "ru_RF")
+        numberOfDays = range.formatted(.components(style: .wide, fields: [.day]).locale(russianLocale))
+    }
+    
+    @IBAction func resoultButtonTapped() {
+        infoLabel.isHidden = false
+        infoLabel.text = "Ты наслаждаешься жизнью уже \(numberOfDays), люби жизнь, развивайся, твори добро!"
+    }
+}
+
